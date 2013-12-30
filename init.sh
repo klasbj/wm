c3443248f4c50333223a52492d927781ba51c7b7
Klas Björkqvist <klas.bjorkqvist@gmail.com>
2013-12-30 16:55:07 +0100

    Add init.sh script.

---
+++ init.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+BASEDIR="$HOME/wm"
+
+echo "Loading statusbars" > $HOME/.log.load_statusbars
+$BASEDIR/statusbars/load_statusbars &>> $HOME/.log.load_statusbars &
+
+# allow the socket to open
+sleep 0.3
+
+[[ -z "$DWMSOCKET" ]] && DWMSOCKET=$BASEDIR/statusbars/dzen.socket
+[[ -z "$DWMBIN" ]] && DWMBIN=$BASEDIR/dwm/dwm
+
+echo "add_area dwm0 0 TOP 1000 CENTER
+add_area dwm1 1 TOP 1000 CENTER" | ncat -U "$DWMSOCKET"
+
+exec $DWMBIN 2> $HOME/.dwm.log | ncat -U "$DWMSOCKET"
